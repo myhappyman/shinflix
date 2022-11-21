b7526a2b66595d7a698542f79c4bf26dca523bde
ParkShinwoo <myhappyman@naver.com>
2022-11-21 15:34:29 +0900

    router를 구성하고 컴포넌트를 만들어서 shinflix header부분 작성해보았다.

--- src/index.tsx
+++ src/index.tsx
@@ -59,7 +59,6 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: 'Source Sans Pro', sans-serif;
-    background: linear-gradient(135deg, #e09, #d0e);
   }
   a {
     text-decoration: none;
