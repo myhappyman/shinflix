3d56fd9c0d5ad68cb9f22ebb0fb88468f8e8f308
ParkShinwoo <myhappyman@naver.com>
2022-11-23 17:13:25 +0900

    슬라이더를 넣어서 실제로 동작하는 느낌으로 UI구성을 해보았다.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,59 +1,114 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
 
 const Wrapper = styled.div`
-    background: #000;
+  background: #000;
 `;
 
 const Loader = styled.div`
-    height: 20vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto:string }>`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 60px;
-    background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)),
-                url( ${props => props.bgPhoto} );
-    background-size: cover;
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
 `;
 
 const Title = styled.h2`
-    font-size: 68px;
-    margin-bottom: 10px;
+  font-size: 68px;
+  margin-bottom: 10px;
 `;
 const Overview = styled.p`
-    font-size: 36px;
-    width: 50%;
+  font-size: 36px;
+  width: 50%;
 `;
 
-function Home(){
-    const {data, isLoading} = useQuery<IGetMoviesResult>(
-        ["movies", "nowPlaying"], 
-        getMovies
-    );
-    console.log(data, isLoading);
-    return (
-        <Wrapper>
-            {isLoading 
-            ? <Loader>Loading...</Loader> 
-            : 
-                <>
-                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-                        <Title>{data?.results[0].title}</Title>
-                        <Overview>{data?.results[0].overview}</Overview>
-                    </Banner>
-                </>
-            }
-        </Wrapper>
-    );
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  margin-bottom: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: #fff;
+  height: 200px;
+  color: red;
+  font-size: 40px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.innerWidth - 10,
+  },
+};
+
+function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => prev + 1);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
