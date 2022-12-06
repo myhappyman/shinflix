a90525f7efff47240e0996a8b0a8fef5b969d55e
ParkShinwoo <myhappyman@naver.com>
2022-12-06 14:04:00 +0900

    banner 수정

--- src/Routes/Components/Banner.tsx
+++ src/Routes/Components/Banner.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { IData } from "../../api";
 import { makeImagePath } from "../../utils";
+import { AiFillCaretRight, AiOutlineInfoCircle } from "react-icons/ai";
 
 const Wrapper = styled.div<{ bgphoto: string }>`
   height: 100vh;
@@ -16,14 +17,14 @@ const Wrapper = styled.div<{ bgphoto: string }>`
 const Title = styled.h2`
   font-size: 68px;
   font-weight: 900;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
 `;
 
 const Overview = styled.p`
   width: 34%;
   font-size: 18px;
   font-weight: 700;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   line-height: 25px;
 `;
 
@@ -32,23 +33,72 @@ const ButtonArea = styled.div`
   gap: 1vw;
 `;
 
-const BannerBtn = styled.button`
-  width: 180px;
-  padding: 15px;
-  font-size: 28px;
-  font-weight: 400;
+interface IBannerBtn {
+  width: string;
+  color: string;
+  bgColor: string;
+  hoverColor: string;
+}
+
+const BannerBtn = styled.button<IBannerBtn>`
+  width: ${(props) => props.width};
+  padding: 18px;
   border-radius: 15px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.bgColor};
+  &:hover {
+    background-color: ${(props) => props.hoverColor};
+  }
 `;
 
+const BtnICon = styled.div`
+  width: 28px;
+  height: 28px;
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  svg {
+    width: 28px;
+    height: 28px;
+  }
+`;
+
+const BtnText = styled.div`
+  font-size: 28px;
+  font-weight: 400;
+`;
 function Banner({ bannerInfo }: { bannerInfo: IData }) {
   return (
     <Wrapper bgphoto={makeImagePath(bannerInfo.backdrop_path || "")}>
       <Title>{bannerInfo.title ? bannerInfo.title : bannerInfo.name}</Title>
       <Overview>{bannerInfo.overview}</Overview>
       <ButtonArea>
-        <BannerBtn>재생</BannerBtn>
-        <BannerBtn>상세 정보</BannerBtn>
+        <BannerBtn
+          width={"170px"}
+          color={"#000"}
+          bgColor={"rgba(255, 255, 255, 1)"}
+          hoverColor={"rgba(255, 255, 255, 0.75)"}
+        >
+          <BtnICon>
+            <AiFillCaretRight />
+          </BtnICon>
+          <BtnText>재생</BtnText>
+        </BannerBtn>
+        <BannerBtn
+          width={"220px"}
+          color={"#fff"}
+          bgColor={"rgba(109, 109, 110, 0.7)"}
+          hoverColor={"rgba(109, 109, 110, 0.4)"}
+        >
+          <BtnICon>
+            <AiOutlineInfoCircle />
+          </BtnICon>
+          <BtnText>상세 정보</BtnText>
+        </BannerBtn>
       </ButtonArea>
     </Wrapper>
   );
