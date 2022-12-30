64209945f9bd4c2bac2cea8eee935deae13bdd7c
Park Shinwoo <myhappyman@naver.com>
2022-12-31 00:41:46 +0900

    left 모달 커버 수정

--- src/Routes/Components/Modal.tsx
+++ src/Routes/Components/Modal.tsx
@@ -14,6 +14,7 @@ const GlobalStyle = createGlobalStyle`
 const Overlay = styled(motion.div)`
   position: fixed;
   top: 0;
+  left: 0;
   width: 100%;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
